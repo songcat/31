7cf04a1e4349f70ea7e9b2e6629c5b3248aba0aa
songcat <songcat@outlook.fr>
2014-07-01 23:46:08 +0200

    inscription form

--- src/ThirtyOne/MemberBundle/Form/Type/RegistrationFormType.php
+++ src/ThirtyOne/MemberBundle/Form/Type/RegistrationFormType.php
@@ -13,55 +13,61 @@ class RegistrationFormType extends BaseType
 
         // custom fields
         $builder->add('civilite', 'choice', array(
-                    'label'=>'Civilité',
-                    'choices'   => array('Monsieur' => 'Monsieur', 'Madame' => 'Madame'),
-                    'multiple'  => false,
-                    'expanded'  => true,
-                ))
-                ->add('username', 'text', array(
-                'label'=>'Nom de famille'
-                ))
-                ->add('firstname', 'text', array(
-                    'required'    => false,
-                    'label'=>'Prénom'
-                ))
-                ->add('nbChildren', 'integer', array(
-                    'label'=>'Nombre d\'enfants'
-                ))
-                ->add('region', 'choice', array(
-                    'choices'   => array(
-                        'Alsace' => 'Alsace',
-                        'Aqutaine' => 'Aqutaine',
-                        'Auvergne' => 'Auvergne',
-                        'Basse-Normandie' => 'Basse-Normandie',
-                        'Bourgogne' => 'Bourgogne',
-                        'Bretagne' => 'Bretagne',
-                        'Centre' => 'Centre',
-                        'Champagne-Ardenne' => 'Champagne-Ardenne',
-                        'Corse' => 'Corse',
-                        'Franche-Comté' => 'Franche-Comté',
-                        'Haute-Normandie' => 'Haute-Normandie',
-                        'Île-de-France' => 'Île-de-France',
-                        'Languedoc-Roussillon' => 'Languedoc-Roussillon',
-                        'Limousin' => 'Limousin',
-                        'Lorraine' => 'Lorraine',
-                        'Midi-Pyrénées' => 'Midi-Pyrénées',
-                        'Nord-Pas-de-Calais' => 'Nord-Pas-de-Calais',
-                        'Pays de la Loire' => 'Pays de la Loire',
-                        'Picardie' => 'Picardie',
-                        'Poitou-Charentes' => 'Poitou-Charentes',
-                        'Provence-Alpes-Côte d\'Azur' => 'Provence-Alpes-Côte d\'Azur',
-                        'Rhône-Alpes' => 'Rhône-Alpes',
-                    ),
-                    'multiple'=>false,
-                    'label'=>'Région'
-                ))
-                ->add('city', 'text', array(
-                    'label'=>'Ville'
-                ))
-                ->add('phonenumber', 'number', array(
-                    'label'=>'Numéro de téléphone'
-                ));
+            'label' => 'Civilité',
+            'choices' => array('Monsieur' => 'Monsieur', 'Madame' => 'Madame'),
+            'multiple' => false,
+            'expanded' => true,
+        ))
+            ->add('username', 'text', array(
+                'label' => 'Nom de famille'
+            ))
+            ->add('firstname', 'text', array(
+                'required' => false,
+                'label' => 'Prénom'
+            ))
+            ->add('nbChildren', 'integer', array(
+                'label' => 'Nombre d\'enfants'
+            ))
+            ->add('region', 'choice', array(
+                'choices' => array(
+                    'Alsace' => 'Alsace',
+                    'Aqutaine' => 'Aqutaine',
+                    'Auvergne' => 'Auvergne',
+                    'Basse-Normandie' => 'Basse-Normandie',
+                    'Bourgogne' => 'Bourgogne',
+                    'Bretagne' => 'Bretagne',
+                    'Centre' => 'Centre',
+                    'Champagne-Ardenne' => 'Champagne-Ardenne',
+                    'Corse' => 'Corse',
+                    'Franche-Comté' => 'Franche-Comté',
+                    'Haute-Normandie' => 'Haute-Normandie',
+                    'Île-de-France' => 'Île-de-France',
+                    'Languedoc-Roussillon' => 'Languedoc-Roussillon',
+                    'Limousin' => 'Limousin',
+                    'Lorraine' => 'Lorraine',
+                    'Midi-Pyrénées' => 'Midi-Pyrénées',
+                    'Nord-Pas-de-Calais' => 'Nord-Pas-de-Calais',
+                    'Pays de la Loire' => 'Pays de la Loire',
+                    'Picardie' => 'Picardie',
+                    'Poitou-Charentes' => 'Poitou-Charentes',
+                    'Provence-Alpes-Côte d\'Azur' => 'Provence-Alpes-Côte d\'Azur',
+                    'Rhône-Alpes' => 'Rhône-Alpes',
+                ),
+                'multiple' => false,
+                'label' => 'Région'
+            ))
+            ->add('address', 'text', array(
+                'label' => 'Adresse'
+            ))
+            ->add('zipcode', 'text', array(
+                'label' => 'Code Postal'
+            ))
+            ->add('city', 'text', array(
+                'label' => 'Ville'
+            ))
+            ->add('phonenumber', 'number', array(
+                'label' => 'Numéro de téléphone'
+            ));
     }
 
     public function getName()
